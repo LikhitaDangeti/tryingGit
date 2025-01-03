58af34f3a59a94edfcae627335fedee039fdbadd
Nymos <122101271+LikhitaDangeti@users.noreply.github.com>
2025-01-03 15:13:18 +0530

    Changed and added GitPractice

--- whatever.js
+++ whatever.js
@@ -1,3 +1,33 @@
-function add(num1, num2) {
-  return num1 + num2;
-}
+// const numbers = [22,53,73,74,35,92,39,17]
+
+// numbers.sort(function (a, b) {
+//   if (a > b) {
+//     return 1;
+//   }
+
+//   if (a === b) {
+//     return 0;
+//   }
+//   return -1;
+// })
+
+// const numbers = [89, 2, 100, 74, 35, 92, 39, 17, 12, 22, 68]
+
+// numbers.sort(function (a, b) {
+//   console.log(a, b)
+//   return a % 2 === 0? 1 : -1;
+// });
+
+// const numbers = [22,53,73,74,35,92,39,17]
+
+// numbers.sort(function (a, b) {
+//   return a-b;
+// })
+
+const numbers = [22,53,73,74,35,92,39,17]
+numbers.
+
+numbers.sort(function (a, b) {
+  return 0;
+})
+
